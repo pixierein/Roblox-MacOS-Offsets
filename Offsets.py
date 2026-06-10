541bd180df4722998c97d5d06c20bc03497ad54a
PixieRein <rainewman099@gmail.com>
2026-06-09 21:27:07 -0300

    Arm64Offsets

---
+++ Offsets.py
@@ -0,0 +1,13 @@
+# Offsets For Latest ARM64 MacOS
+
+# will add more later
+pairs = [
+    ('print', 0x1016ae860),
+    ('getstate', 0x101790918),
+    ('teleport', 0x1022efffc),
+    ('private', 0x1002c0acc),
+    ('fireclickdetector', 0x1018e367c),
+    ('appendtouch', 0x1024cd374),
+    ('context', 0x100e6ac9c),
+    ('taskscheduler', 0x105868248),
+]
